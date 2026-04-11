31eb89aacca54530b706e621323e3067b8b7b564
ElamanAgibaev <agibaev.17@mail.ru>
2026-04-12 00:38:52 +0500

    updateHandler

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	todo "todo-app"
+	"todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
