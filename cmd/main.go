8017138287c00eb593380e488b351bfa4cc1193e
ElamanAgibaev <agibaev.17@mail.ru>
2026-04-13 00:08:27 +0500

    auth

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
-	"log"
+	"github.com/joho/godotenv"
+	_ "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	_ "github.com/sirupsen/logrus"
+
+	"os"
+
 	todo "todo-app"
 	"todo-app/pkg/handler"
+	"todo-app/pkg/repository"
+	"todo-app/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
-	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occurred while running http server: %s", err.Error())
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := InitConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("failed to initializing db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		logrus.Fatalf("error occurred while running http server: %s", err.Error())
+	}
+}
+
+func InitConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
